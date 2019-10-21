85993f0ef89abccf0b146c8e6715a8594fc1b38d
liufeng731023 <482024409@qq.com>
2019-10-21 15:42:28 +0000

    访问策略

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
     public function create(){
         return view('sessions.create');
     }
@@ -20,7 +26,8 @@ class SessionsController extends Controller
 
         if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','登录成功,欢迎回来');
-            return redirect()->route('users.show',Auth::user());
+            $fallback = route('users.show',Auth::user());
+            return redirect()->intended($fallback);
 
         }else{
             session()->flash('danger','登录失败，抱歉');
