e9072cbc501af0da57c85425978f6c0e0fe8c342
liufeng731023 <482024409@qq.com>
2019-10-21 14:40:42 +0000

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -30,6 +31,7 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
+        Auth::login($user);
         session()->flash('success','注册成功，欢迎使用');
         return redirect()->route('users.show',[$user]);
     }
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -15,6 +15,6 @@
 
       </div>
     </div>
-
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
   </body>
 </html>
